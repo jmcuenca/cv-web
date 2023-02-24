7a2e616cd24bba134728e6fbe9311326d0d3c678
Juan Cuenca <juan.cuenca@asistecooper.com.ec>
2023-02-24 16:37:07 -0500

    inicio

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
+
+// Remove this if you're not using Fullcalendar features
+
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+  },
+  transpilePackages: [
+    '@fullcalendar/common',
+    '@fullcalendar/core',
+    '@fullcalendar/react',
+    '@fullcalendar/daygrid',
+    '@fullcalendar/list',
+    '@fullcalendar/timegrid'
+  ],
+  experimental: {
+    esmExternals: false
+  },
+  webpack: config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
+    }
+
+    return config
+  }
+}
